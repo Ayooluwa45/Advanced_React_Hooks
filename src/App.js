30cb47a3b38384aa49dcdb6cd3ba33bdae523732
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-03-10 09:45:13 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import UseRefExample1 from './components/UseRefExample1';
 import UseRefExample2 from './components/UseRefExample2';
 import UseRefExample3 from './components/UseRefExample3';
+import UseMemoExample from './components/UseMemoExample'; 
+import CustomHookExample from './components/CustomHookExample';
 
 function App() {
   return (
     <div className='container mt-5'>
-<UseRefExample1/>
+ <UseRefExample1/>
 <UseRefExample2/>
 <UseRefExample3/>
+<UseMemoExample/> 
+<CustomHookExample/>
     </div>
     
   );
